c7a668e11876ca9d71d2b057586210303be8bdd2
A2510 <aakashgorai25@gmail.com>
2024-09-22 19:44:40 +0530

    Added default admin user

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,7 @@ python manage.py migrate --no-input
 # Collect static files
 python manage.py collectstatic --no-input --clear
 
+echo "from authentication.models import CustomUser; CustomUser.objects.create_superuser('admin@gmail.com', '1234')" | python manage.py shell
+
 # Start server
 daphne -b 0.0.0.0 -p 8000 project.asgi:application
